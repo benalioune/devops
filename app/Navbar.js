4c2018d8eb5baa905c936f710e26eda4f818404d
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2024-03-27 14:58:15 +0100

    fix: active page navbar

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,22 +1,25 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation'
 import Image from 'next/image'; // Import de Image depuis Next.js
 
 export default function Navbar() {
+const pathname = usePathname()
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
                 <Link href="/">
-                    <span href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Accueil</span>
+                    <span href="#" className={`link ${pathname === '/' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium ' : 'text-white rounded-md px-3 py-2 text-sm font-medium '}`}>Accueil</span>
                 </Link>
                 <Link href="/crypto">
-                    <span href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Crypto Monnaie</span>
+                    <span href="#" className={`link ${pathname === '/crypto' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium ' : 'text-white rounded-md px-3 py-2 text-sm font-medium '}`}>Crypto Monnaie</span>
                 </Link>
                 <Link href="/ether">
-                    <span href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Conversion</span>
+                    <span href="#" className={`link ${pathname === '/ether' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium ' : 'text-white rounded-md px-3 py-2 text-sm font-medium '}`}>Conversion</span>
                 </Link>
                 <Link href="/wallet">
-                    <span href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Wallet</span>
+                    <span href="#" className={`link ${pathname === '/wallet' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium ' : 'text-white rounded-md px-3 py-2 text-sm font-medium '}`}>Wallet</span>
                 </Link>
             </div>
         </div>
