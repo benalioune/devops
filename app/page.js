42f257d7c34318ff8e9fdfa9f28058a1bc049567
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2024-03-27 12:48:34 +0100

    Add Navbar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from 'next/link';
+import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="">
       {/* Navigation */}
+      <Navbar/>
       
+      <div className="flex flex-col items-center justify-between min-h-screen p-24">
 
       {/* Logo */}
       <div className="relative flex items-center mb-16 before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -29,15 +32,7 @@ export default function Home() {
           Les principaux avantages de DevOps incluent une plus grande agilité et flexibilité dans le développement logiciel, des déploiements plus rapides, une meilleure qualité du code, une réduction des coûts et une amélioration de la collaboration entre les équipes. En adoptant DevOps, les organisations peuvent améliorer leur efficacité opérationnelle et offrir des produits logiciels de meilleure qualité à leurs clients.
         </p>
       </section>
-
-      <nav className="max-w-5xl w-full flex justify-center items-center font-mono text-sm mb-8 lg:mb-0">
-        <Link href="/crypto">
-          <button className="border border-transparent rounded-md shadow-sm py-2 px-4 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl text-base font-medium text-white hover:from-zinc-300 lg:py-2 lg:text-lg lg:px-8">Crypto Monnaie</button>
-        </Link>
-        <Link href="/ether">
-          <button className="ml-4 border border-transparent rounded-md shadow-sm py-2 px-4 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl text-base font-medium text-white hover:from-zinc-300 lg:py-2 lg:text-lg lg:px-8">Conversion</button>
-        </Link>
-      </nav>
+      </div>
     </main>
   );
 }
